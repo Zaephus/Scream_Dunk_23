a84e56aa78a35b991f10788c9588fb11a7318469
Marc Rozendaal <rozendaalmarc@gmail.com>
2023-07-17 15:44:07 +0200

    Added basic level one functionality

--- Zomblympics/Assets/01_Scripts/Level One/PlayerController.cs
+++ Zomblympics/Assets/01_Scripts/Level One/PlayerController.cs
@@ -6,16 +6,46 @@ using UnityEngine;
 namespace LevelOne {
 
     public class PlayerController : MonoBehaviour {
+
+        [SerializeField, ReadOnly]
+        private float rateOfChange = 0.0f;
+        [SerializeField]
+        private float rotateForceModifier = 10.0f;
+
+        public Vector2 force;
+
+        [SerializeField]
+        private GameObject disc;
+
+        private Rigidbody2D body;
+        private FixedJoint2D joint;
         
         private void Start() {
-            
+            body = GetComponent<Rigidbody2D>();
+            joint = GetComponent<FixedJoint2D>();
         }
 
         private void Update() {
+            RotatePlayer();
+            if(joint != null) {
+                force = joint.reactionForce;
+                Debug.Log(force.magnitude);
+            }
+
+            if(Input.GetMouseButtonDown(0)) {
+                joint.enabled = false;
+            }
+        }
+
+        private void RotatePlayer()  {
 
             Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -Camera.main.transform.position.z));
-            transform.right = (mousePos - transform.position).normalized;
-            
+            Vector3 targetDir = (mousePos - transform.position).normalized;
+
+            rateOfChange = Mathf.Deg2Rad * Vector3.SignedAngle(targetDir, transform.right, -Vector3.forward);
+
+            body.AddTorque(rateOfChange * rotateForceModifier * Time.deltaTime, ForceMode2D.Force);
+
         }
         
     }
